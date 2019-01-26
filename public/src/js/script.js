fa7082a57d2fdd0c933b5467a33223c7d47d30fc
gazsi.bence <gazsi.bence@virgo.hu>
2019-01-26 15:42:46 +0100

    lightgallery.js

---
+++ public/src/js/script.js
@@ -0,0 +1,15 @@
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(function () {
+  lightGallery(document.querySelector('.gallery-1'))
+  lightGallery(document.querySelector('.gallery-2'))
+  lightGallery(document.querySelector('.gallery-3'))
+  lightGallery(document.querySelector('.gallery-4'))
+  lightGallery(document.querySelector('.gallery-5'))
+})
